33eb6012ba57f5d506cc3c10622c28439aec5aec
lhomitenzing202 <tenzinglhomi15@gmail.com>
2024-03-27 15:54:48 +0545

    admin pannel setup for product list done

--- src/Components/ListProduct/Listproduct.jsx
+++ src/Components/ListProduct/Listproduct.jsx
@@ -1,8 +1,60 @@
+import { useEffect, useState } from 'react'
 import './ListProduct.css'
+import cross_icon from '../../assets/cross_icon.png'
 
 const Listproduct = () => {
+
+  const[allProducts,setAllProducts]=useState([]);
+
+  const fetchInfo =async()=>{
+    await fetch('http://localhost:4000/allproducts')
+    .then((res)=>res.json())
+    .then((data)=>{setAllProducts(data)});
+  }
+
+  useEffect(()=>{
+    fetchInfo();
+    },[])
+
+    const removeProduct=async(id)=>{
+      await fetch('http://localhost:4000/removeProduct',{
+        method:'POST',
+        headers:{
+            Accept: 'application/json',
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({id:id})
+    })
+    await fetchInfo();
+    }
   return (
-    <div>Listproduct</div>
+    <div className="list-product">
+      <h1>All Products List</h1>
+      <div className="listproduct-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Old Price</p>
+        <p>New Price</p>
+        <p>Category</p>
+        <p>Remove</p>
+      </div>
+      <div className="listproduct-allproducts">
+        <hr />
+        {allProducts.map((product,index)=>{
+          return <><div key={index} className="listproduct-format-main listproduct-format">
+            <img className='listproduct-product-icon' src={product.image} alt="" />
+            <p>{product.name}</p>
+            <p>Nrs:{product.old_price}</p>
+            <p>Nrs:{product.new_price}</p>
+            <p>{product.category}</p>
+            <img onClick={()=>{removeProduct(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
+          </div>
+          <hr />
+          </>
+        })}
+
+      </div>
+    </div>
   )
 }
 
